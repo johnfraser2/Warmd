2088a5e8220946a4d7295eac65175bff9fabf08e
Frédéric Julian <f.julian@oodrive.com>
2020-06-06 22:35:46 +0200

    fix another typo

--- gen_code.sh
+++ gen_code.sh
@@ -2,3 +2,4 @@
 
 flutter pub run easy_localization:generate
 flutter pub run easy_localization:generate -f keys -o locale_keys.g.dart
+sed -i -- "s/Map<String,dynamic>/Map<String,String>/g" ./lib/generated/codegen_loader.g.dart
